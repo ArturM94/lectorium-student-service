abcc7535221f70cb8502224a3b4c20b16a366591
Artur Manukian <manukian.artur.94@gmail.com>
2021-03-15 18:17:18 +0200

    feat: add config for .env

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       transport: Transport.KAFKA,
       options: {
         client: {
-          brokers: ['localhost:9092'],
+          brokers: [`localhost:${process.env.KAFKA_PORT}`],
         },
       },
     },
