cf34ea844f6d58d79df8718d7f8daa5f43af6201
Artur Manukian <manukian.artur.94@gmail.com>
2021-03-19 14:59:11 +0200

    feat: connect Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), StudentsModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        autoLoadEntities: true,
+        synchronize: process.env.NODE_ENV !== 'production',
+      }),
+    }),
+    ConfigModule.forRoot(),
+    StudentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
