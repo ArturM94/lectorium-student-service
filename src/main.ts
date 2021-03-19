2c8c88ac853db86a0e7df4d759f5805d60b1380e
Artur Manukian <manukian.artur.94@gmail.com>
2021-03-19 16:33:11 +0200

    feat: set up basic service
    * feat: turn app to microservice
    
    * feat: create students module
    
    * feat: add config for .env
    
    * feat: extend microservice options
    
    * feat: connect Postgres
    
    * feat: add ormconfig

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: [`${process.env.KAFKA_HOST}:${process.env.KAFKA_PORT}`],
+        },
+        consumer: {
+          groupId: 'students-consumer',
+        },
+      },
+    },
+  );
+  await app.listen(() => console.log('Microservice is listening'));
 }
 bootstrap();
